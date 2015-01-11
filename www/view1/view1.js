40bdbcfe166bc7ab2daf588764eb101ab5eb2c60
Junyong <zengjunyong@gmail.com>
2015-01-12 00:00:02 +0800

    call contracts api

--- www/view1/view1.js
+++ www/view1/view1.js
@@ -2,13 +2,28 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
 
-.controller('View1Ctrl', [function() {
+  .controller('View1Ctrl', ['$scope', function ($scope) {
+    function onSuccess(contacts) {
+      for (var i = 0; i < contacts.length; i++) {
+        console.log(contacts[i]);
+      }
+    }
 
-}]);
+    var options = new ContactFindOptions();
+    options.filter = "";
+    options.multiple = true;
+    var filter = ["displayName", "phoneNumbers"];
+    navigator.contacts.find(filter, onSuccess, onError, options);
+  }]);
+
+
+function onError(contactError) {
+  alert('Oops Something went wrong!');
+}
